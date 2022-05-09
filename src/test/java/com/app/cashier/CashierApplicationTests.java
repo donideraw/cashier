9fb643e124f78fc3aedc58571843e833924900d7
donideraw <donideraw@gmail.com>
2022-05-09 09:32:20 +0700

    initial project

---
+++ src/test/java/com/app/cashier/CashierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.cashier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
